cd09257037f5264d7c7447c2eb6ad08a38ffad70
fahadalijs <fahad9827022@gmail.com>
2024-05-21 15:08:02 +0500

    Js date and time

---
+++ app.js
@@ -0,0 +1,147 @@
+// console.log("Js-date and time");
+
+
+// let now = new Date();
+// console.log(now);
+
+// let dt = new Date(1000);
+// console.log(dt);
+
+// let newDate = new Date("2029-09-30");
+// console.log(newDate);
+
+// Let newDate = new Date(year , month , date , hours , minutes , seconds , milliseconds);
+
+// let newDate = new Date(3020 , 4 , 6 , 9 , 3 , 2 , 34);
+// console.log(newDate);
+
+
+// let yr =  newDate.getFullYear();
+// console.log("The year is" , yr);
+
+// let date =  newDate.getDate();
+// console.log("The dt is" , date);
+
+// let month =  newDate.getMonth();
+// console.log("The month is" , month);
+
+// let hours =  newDate.getHours();
+// console.log("The hours is" , hours);
+
+
+
+
+// newDate.setDate(35);
+// console.log(newDate);
+
+// newDate.setMinutes(35);
+// console.log(newDate)
+
+
+// setInterval(updateTime, 1000);
+
+// function updateTime() {
+//     time.innerHTML = new Date(); 
+// }
+
+
+
+
+// let currentDate = new Date();
+// console.log(currentDate.toLocaleTimeString());
+// // let dayNames = ["Sun" , "Mon" , "Tues" , "Wed" , "Tus" , "Friday" , "Sat"];
+// console.log(currentDate.getDate());
+// console.log(currentDate.getHours());
+// console.log(currentDate.getMinutes());
+// console.log(currentDate.getSeconds());
+// console.log(currentDate.getMilliseconds());
+// console.log(currentDate.getTime());
+
+
+
+
+// const birthDate = new Date("June 27 2003");
+// console.log(birthDate.getTime());
+// 
+
+
+
+// const hyderabadi = new Date("May 26 , 2004").getTime();
+// const currentDate = new Date().getTime();
+// const remainingMillisecond = currentDate - hyderabadi
+
+// console.log(remainingMillisecond / 1000 / 60 / 60 / 24 / 30 / 12);
+
+
+
+
+
+// let currentDate = new Date("May 26, 2004 07:25:00");
+// let setYear = currentDate.setMonth(6);
+// console.log(setYear);
+// console.log(currentDate.toLocaleTimeString());
+
+
+
+
+// const date = document.querySelector("#date");
+
+// function getDateForminput(){
+//     console.log(date.value);
+//     const currentDate = new Date(date.value);
+//     console.log(currentDate);
+//     input = " "
+// }
+
+
+
+
+
+
+
+// const date = new Date();
+// console.log(typeof(date));
+
+
+
+// let a = 10; global variable
+
+// function hello() {
+    // let b = 10 local variable
+    // console.log(a);
+// }
+// console.log(b); error
+// hello()
+
+
+
+const inputdedauser = prompt("input daa shahbashi!");
+const date = new Date();
+console.log(date);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
